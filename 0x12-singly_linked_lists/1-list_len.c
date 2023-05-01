304c61fda735154b6d3d9257d67b999bb809f86c
teqbaddie <oyinkansolashoroye@gmail.com>
2023-04-30 18:28:33 -0700

    updated file

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - number of elements list
+ * @h: pointer to structure
+ * Return: length.
+ */
+
+size_t list_len(const list_t *h)
+{
+
+unsigned int i = 0;
+
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
